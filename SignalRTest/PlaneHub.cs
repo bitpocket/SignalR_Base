f69cbafe17d0190778c0cd8fb82bb89434a5c1c3
Cendrzak, Wojciech (FP) <wcendrzak@future-processing.com>
2015-12-09 15:31:11 +0100

    initial

---
+++ SignalRTest/PlaneHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace SignalRTest
+{
+	public class PlaneHub : Hub
+	{
+		public PlaneHub()
+		{
+			timer = new Timer(timercalback, null, 0, 1000);
+		}
+
+		private void timercalback(object state)
+		{
+			tick();
+		}
+
+		Timer timer;
+
+		public static int ticker = 0;
+
+		public void tick()
+		{
+			ticker++;
+
+			Res res = new Res() { Ticker = ticker, GeneratedDate = DateTime.Now };
+			Clients.All.ticker(res);
+		}
+	}
+
+	public class Res
+	{
+		public int Ticker;
+		public DateTime GeneratedDate;
+	}
+}
